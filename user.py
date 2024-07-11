034b908703d9d77bf608c91db98fc604ab06647e
yoominchoi <letsfly612@gmail.com>
2024-07-11 14:50:29 -0700

    tables for shooter location and one alert message

--- user.py
+++ user.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 from common import fetch_users, update_incident, fetch_incidents, fetch_shooter_location
+# import json
 
 # Sidebar for selecting user
 # user = st.sidebar.selectbox("Select User", ["John Smith", "Sarah Kim"])
@@ -10,6 +11,8 @@ user_dict = {user_id: (name, user_type) for user_id, name, user_type in users}
 user_id = st.sidebar.selectbox("Select User", list(user_dict.keys()))
 user_name, user_type = user_dict[user_id]
 
+#######################
+# Admin User
 if user_type == 'A':
     st.title("Admin Dashboard")
 
@@ -18,33 +21,45 @@ if user_type == 'A':
     # Incident Details
     st.header("Incident Details")
     incidents = fetch_incidents()
+    # incident[0]: timestamp, incident[1]: updated_by, incident[2]: shooter_location, incident[3]: alert_msg
     if incidents:
         df = pd.DataFrame(incidents, columns=["Timestamp", "Updated By", "Shooter Location", "Alert Message"])
-        alert_msg = df['Alert Message']
-        print(alert_msg)
+        df = df[df["Shooter Location"].notnull()]  # Filter out rows where Shooter Location is null
         df.drop(columns=['Alert Message'], axis=1, inplace=True)
-        st.table(df)
+        if not df.empty:
+            st.table(df.head(3))
+        else:
+            st.write("No incident details available.")
     else:
         st.write("No incident details available.")
 
-    # Message to users
+    # Already sent message
     st.header("Messages to Users")
     messages = fetch_incidents()
-    if messages:
-        for msg in messages:
-            st.write(f"{msg[3]} ({msg[0]} - {msg[1]})")
+    # find the most recent message that is not null
+    for msg in messages:
+        if msg[3] is not None:
+            msg = msg
+            break
+    if msg:
+        st.write(f"{msg[3]} ({msg[0]})")
     else:
         st.write("No messages available.")
-
+    
+    # Updating an alert message
     message = st.text_area("Send Message to General Users")
     if st.button("Send"):
         update_incident(user_id, "alert_msg", message)
         st.success("Message sent!")
-
+    
+    # Updating shooter's location
     location = st.text_input("Update Shooter's Location")
     if st.button("Update Location"):
         update_incident(user_id, "shooter_location", location)
         st.success("Shooter's location updated!")
+
+#######################
+# General User
 elif user_type == 'G':
     st.title(f"General User {user_name}")
 
@@ -53,21 +68,25 @@ elif user_type == 'G':
     st.header("Incident Details")
     incidents = fetch_incidents()
     if incidents:
-        # df = pd.DataFrame(incidents, columns=["Timestamp", "Updated By", "Shooter Location", "Alert Message"])
-        # st.table(df)
         df = pd.DataFrame(incidents, columns=["Timestamp", "Updated By", "Shooter Location", "Alert Message"])
-        alert_msg = df['Alert Message']
+        df = df[df["Shooter Location"].notnull()]  # Filter out rows where Shooter Location is null
         df.drop(columns=['Alert Message'], axis=1, inplace=True)
-        st.table(df)
+        df.drop(columns=['Updated By'], axis=1, inplace=True)
+        if not df.empty:
+            st.table(df.head(3))
     else:
         st.write("No incident details available.")
 
     # Message from Admin
     st.header("Messages from Admin")
     messages = fetch_incidents()
-    if messages:
-        for msg in messages:
-            st.write(f"{msg[3]} ({msg[0]} - {msg[1]})")
+    # find the most recent message that is not null
+    for msg in messages:
+        if msg[3] is not None:
+            msg = msg
+            break
+    if msg:
+        st.write(f"{msg[3]} ({msg[0]})")
     else:
         st.write("No messages available.")
 
