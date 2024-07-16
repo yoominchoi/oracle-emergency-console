32fe69691de5c9902783054d4523609a83d5bab3
yoominchoi <letsfly612@gmail.com>
2024-07-15 21:50:13 -0700

    safe & urgent added

--- user.py
+++ user.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from common import fetch_users, update_incident, fetch_incidents
+from common import fetch_users, update_incident, fetch_incidents, update_user
 # import json
 
 # Sidebar for selecting user
@@ -9,14 +9,13 @@ users = fetch_users()
 user_dict = {user_id: (name, user_type) for user_id, name, user_type in users}
 
 user_id = st.sidebar.selectbox("Select User", list(user_dict.keys()))
+incident_id = st.sidebar.number_input('Incident ID', min_value=1)
 user_name, user_type = user_dict[user_id]
 
 #######################
 # Admin User
 if user_type == 'A':
     st.title("Admin Dashboard")
-
-    incident_id = st.sidebar.number_input('Incident ID', min_value=1)
 
     # Incident Details
     st.header("Incident Details")
@@ -49,21 +48,24 @@ if user_type == 'A':
     # Updating an alert message
     message = st.text_area("Send Message to General Users")
     if st.button("Send"):
-        update_incident(user_id, "alert_msg", message)
+        update_incident(1, user_id, "alert_msg", message)
         st.success("Message sent!")
     
     # Updating shooter's location
     location = st.text_input("Update Shooter's Location")
     if st.button("Update Location"):
-        update_incident(user_id, "shooter_location", location)
-        st.success("Shooter's location updated!")
+        if user_id and incident_id and location:
+            print('incident_id', incident_id)
+            update_incident(incident_id, user_id, "shooter_location", location)
+            # update_shooter_location(user_id, incident_id, location)
+            st.success("Shooter's location updated!")
+        else:
+            st.error("Error occurred.")
 
 #######################
 # General User
 elif user_type == 'G':
     st.title(f"General User {user_name}")
-
-    incident_id = st.sidebar.number_input('Incident ID', min_value=1)
 
     st.header("Incident Details")
     incidents = fetch_incidents()
@@ -93,5 +95,23 @@ elif user_type == 'G':
     # Updating Shooter's Location
     location = st.text_input("Update Shooter's Location")
     if st.button("Update"):
-        update_incident(user_id, "shooter_location", location)
+        update_incident(incident_id, user_id, "shooter_location", location)
         st.success("Shooter's location updated!")
+
+    # Check if it is safe
+    st.header("Safe?")
+    if st.button("safe_Yes"):
+        update_user(user_id, "is_safe", 'Y')
+        st.success("Safe count successful")
+    elif st.button("safe_No"):
+        update_user(user_id, "is_safe", 'N')
+        st.success("Unsafe count successful")
+
+    # Check if it is urgent
+    st.header("Urgent?")
+    if st.button("urgent_Yes"):
+        update_user(user_id, "is_urgent", 'Y')
+        st.success("Safe count successful")
+    elif st.button("urgent_No"):
+        update_user(user_id, "is_urgent", 'N')
+        st.success("Unsafe count successful")
