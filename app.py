c7b80c1403994b2e5f1af04d312ce04b8db97a84
yoominchoi <letsfly612@gmail.com>
2024-07-22 00:38:39 -0700

    cleaning up codes

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import register
 import login
 import dashboard
-# import create
 
 def main():
     if "user" not in st.session_state:
@@ -24,13 +23,6 @@ def main():
             elif page == "Register":
                 register.main()
     else:
-        # st.sidebar.title("Navigation")
-        # page = st.sidebar.radio("Go to", ["Dashboard", "Report a New Incident" ])
-        # if page == "Report a New Incident":
-        #     create.main()
-        # else:
-            # query_params = st.query_params()
-            # if "incident_id" in query_params:
         dashboard.main()
 
 
